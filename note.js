48a3b474544b6fb82ffab83fd3468db2774f0d87
pisanggoreng <7ofpentacles@gmail.com>
2017-02-22 15:55:03 +0700

    add seuelize-cli

---
+++ note.js
@@ -0,0 +1,11 @@
+Urls
+  url
+  shortener
+  clickCount
+
+sequelize model:create --name Url --attributes 'url:string, shortener:string, clickCount:integer'
+
+https://gentle-basin-25080.herokuapp.com/
+
+database
+postgresql-octagonal-10516
