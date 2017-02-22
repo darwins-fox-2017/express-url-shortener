7cce4c4433a826470e008ebe40f0b5433cb937b6
Diky Arga <dikyarga.id@gmail.com>
2017-02-22 16:20:41 +0700

    remove another weird code

--- helpers/app.js
+++ helpers/app.js
@@ -2,8 +2,6 @@ let moment = require('moment')
 
 module.exports = {
   beautifyUrl : function(code){
-    let correctDomainLink = getDomainUrl(code); // 'https://domain.com'
-    console.log(correctDomainLink);
-    return correctDomainLink + code
+    return code
   }
 }
